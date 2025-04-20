a7473608d971f82ca422750500b43e07a26e8af8
Wooolfin <122937648+Wooolfin@users.noreply.github.com>
2025-04-20 19:31:23 -0300

    Criada GET, POST com API RESTFUL em python e MySQL

---
+++ api/services/pedido_service.py
@@ -0,0 +1,22 @@
+from database import get_connection
+
+def get_pedidos():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM pedido")
+    pedidos = cursor.fetchall()
+    conn.close()
+    return pedidos
+
+def add_pedido(cliente):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        INSERT INTO pedido (cliente)
+        VALUES (%s)
+    """, (cliente.cliente,))
+    pedido_id = cursor.lastrowid
+    conn.commit()
+    conn.close()
+
+    return {"pedido_id": pedido_id, "status": "Pedido criado com sucesso"}
