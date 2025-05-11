008fd02cd650c99d4382c014ed5548baf7fdffde
Wooolfin <122937648+Wooolfin@users.noreply.github.com>
2025-05-11 19:43:30 -0300

    feat - Consultando atraves de API RESTFUL

---
+++ src/app/services/classificacao/classificacao.model.ts
@@ -0,0 +1,10 @@
+export interface Classificacao {
+  id_classificacao: number;
+  nome_classificacao: string;
+  usa_tamanho: boolean;
+}
+
+export interface Tamanho {
+  descricao_tamanho: string;
+  preco: number;
+}
