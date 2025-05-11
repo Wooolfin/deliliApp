360cb02463345bc52b198195228c5277c6c7281b
Wooolfin <122937648+Wooolfin@users.noreply.github.com>
2025-05-11 19:41:41 -0300

    feat - Direcionando para nova tela de classificacao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'cadastro',
+    redirectTo: 'classificacao',
     pathMatch: 'full',
   },
   {
@@ -15,5 +15,9 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () =>
       import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'classificacao',
+    loadComponent: () => import('./classificacao/classificacao.page').then( m => m.ClassificacaoPage)
   },
 ];
